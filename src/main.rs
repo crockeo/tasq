c3d2fd47dd603f3d51f1a5f675d784b29263ba70
Cerek Hillen <cerekh@gmail.com>
2023-03-16 14:55:13 -0700

    start implementing commands

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::rc::Rc;
 
+use anyhow::anyhow;
 use serde::Deserialize;
 use serde::Serialize;
 use structopt::StructOpt;
+use uuid::Uuid;
 
 // CLI interface to add/remove tasks from a graph database
 // and then find the next thing you should do for a particular set of tasks
@@ -34,12 +36,16 @@ fn main() -> anyhow::Result<()> {
     graph.save_default()
 }
 
-fn add(args: AddArgs, graph: Graph) -> anyhow::Result<Graph> {
-    todo!()
+fn add(args: AddArgs, mut graph: Graph) -> anyhow::Result<Graph> {
+    let node = Node::new();
+    println!("{}", node.id.to_string());
+    graph.add(node);
+    Ok(graph)
 }
 
-fn connect(args: ConnectArgs, graph: Graph) -> anyhow::Result<Graph> {
-    todo!()
+fn connect(args: ConnectArgs, mut graph: Graph) -> anyhow::Result<Graph> {
+    graph.connect(args.from, args.to)?;
+    Ok(graph)
 }
 
 fn edit(args: EditArgs, graph: Graph) -> anyhow::Result<Graph> {
@@ -47,7 +53,38 @@ fn edit(args: EditArgs, graph: Graph) -> anyhow::Result<Graph> {
 }
 
 fn show(args: ShowArgs, graph: Graph) -> anyhow::Result<Graph> {
-    todo!()
+    let to_show = if let Some(root_id) = args.root {
+        vec![root_id]
+    } else {
+        graph.get_roots()
+    };
+
+    for node in to_show.into_iter() {
+        show_subgraph(&graph, node)?;
+    }
+
+    Ok(graph)
+}
+
+fn show_subgraph(graph: &Graph, root: NodeID) -> anyhow::Result<()> {
+    let mut seen = BTreeSet::new();
+    let mut stack = vec![(root, 0)];
+    while stack.len() > 0 {
+        let (next, indentation) = stack.pop().unwrap();
+        if seen.contains(&next) {
+            continue;
+        }
+        seen.insert(next);
+
+        for _ in 0..indentation {
+            print!(" ");
+        }
+        println!("{}", graph.get_node(next)?.short_repr());
+
+        // TODO: find next
+    }
+
+    Ok(())
 }
 
 fn next(args: NextArgs, graph: Graph) -> anyhow::Result<Graph> {
@@ -67,18 +104,24 @@ enum Opt {
 struct AddArgs {}
 
 #[derive(Debug, StructOpt)]
-struct ConnectArgs {}
+struct ConnectArgs {
+    from: Uuid,
+    to: Uuid,
+}
 
 #[derive(Debug, StructOpt)]
 struct EditArgs {}
 
 #[derive(Debug, StructOpt)]
-struct ShowArgs {}
+struct ShowArgs {
+    #[structopt(short = "r", long = "root")]
+    root: Option<NodeID>,
+}
 
 #[derive(Debug, StructOpt)]
 struct NextArgs {}
 
-type NodeID = usize;
+type NodeID = Uuid;
 
 #[derive(Deserialize, Serialize)]
 pub struct Node {
@@ -89,6 +132,22 @@ pub struct Node {
     pub due: Option<()>,
 }
 
+impl Node {
+    pub fn new() -> Self {
+        Node {
+            id: Uuid::new_v4(),
+            title: "".to_string(),
+            description: "".to_string(),
+            scheduled: None,
+            due: None,
+        }
+    }
+
+    pub fn short_repr(&self) -> String {
+        self.id.to_string()
+    }
+}
+
 #[derive(Default, Deserialize, Serialize)]
 pub struct Graph {
     nodes: BTreeMap<NodeID, Rc<Node>>,
@@ -97,12 +156,6 @@ pub struct Graph {
 }
 
 impl Graph {
-    fn default_path() -> anyhow::Result<PathBuf> {
-        let mut graph_file = std::env::current_dir()?;
-        graph_file.push("graph.json");
-        Ok(graph_file)
-    }
-
     pub fn load_default() -> anyhow::Result<Graph> {
         Self::load(&Self::default_path()?)
     }
@@ -116,7 +169,7 @@ impl Graph {
     }
 
     pub fn save_default(&self) -> anyhow::Result<()> {
-	self.save(&Self::default_path()?)
+        self.save(&Self::default_path()?)
     }
 
     pub fn save(&self, path: &Path) -> anyhow::Result<()> {
@@ -131,15 +184,34 @@ impl Graph {
         self.nodes.insert(node.id, node);
     }
 
-    pub fn connect(&mut self, from: NodeID, to: NodeID) {
+    pub fn connect(&mut self, from: NodeID, to: NodeID) -> anyhow::Result<()> {
+        if !self.nodes.contains_key(&from) {
+            return Err(anyhow!("Missing node {}", from.to_string()));
+        }
+        if !self.nodes.contains_key(&to) {
+            return Err(anyhow!("Missing node {}", to.to_string()));
+        }
+
         self.edges.insert(from, to);
         self.roots.remove(&to);
+        Ok(())
     }
 
-    pub fn get_roots(&self) -> Vec<Rc<Node>> {
-        self.roots
-            .iter()
-            .map(|root| self.nodes.get(root).expect("missing node for root").clone())
-            .collect()
+    pub fn get_node(&self, id: NodeID) -> anyhow::Result<Rc<Node>> {
+        Ok(self
+            .nodes
+            .get(&id)
+            .ok_or(anyhow!("Missing node ID {}", id))?
+            .clone())
+    }
+
+    pub fn get_roots(&self) -> Vec<NodeID> {
+        self.roots.iter().map(Uuid::clone).collect()
+    }
+
+    fn default_path() -> anyhow::Result<PathBuf> {
+        let mut graph_file = std::env::current_dir()?;
+        graph_file.push("graph.json");
+        Ok(graph_file)
     }
 }
